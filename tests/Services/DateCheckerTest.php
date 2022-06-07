e22ab25a003d649a938334fa59ccd9b5aba65696
florianlangard <florian.langard@gmail.com>
2022-06-07 16:43:04 +0200

    added Tests for DateChecker Service

--- tests/Services/DateCheckerTest.php
+++ tests/Services/DateCheckerTest.php
@@ -2,9 +2,57 @@
 
 namespace App\Tests\Services;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Service\DateChecker;
+use DateTime;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Repository\EventRepository;
 
-class DateCheckerTest extends WebTestCase {
-    
+class DateCheckerTest extends KernelTestCase {
+
+    public function testDateCheckerWithValidRaceDate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $dateChecker = $container->get(DateChecker::class);
+        $eventRepository = $container->get(EventRepository::class);
+        $testEvent = $eventRepository->findOneBy(['round' => 19, 'season' => '2022']);
+        $validDate = $dateChecker->checkDate($testEvent->getDate());
+        $this->assertTrue($validDate);
+    }
+
+    public function testDateCheckerWithValidQualifyingDate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $dateChecker = $container->get(DateChecker::class);
+        $eventRepository = $container->get(EventRepository::class);
+        $testEvent = $eventRepository->findOneBy(['round' => 19, 'season' => '2022']);
+        $validDate = $dateChecker->checkDate($testEvent->getQualifyingDate());
+        $this->assertTrue($validDate);
+    }
+
+    public function testDateCheckerWithNotValidRaceDate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $dateChecker = $container->get(DateChecker::class);
+        $eventRepository = $container->get(EventRepository::class);
+        $testEvent = $eventRepository->findOneBy(['round' => 0, 'season' => '2022']);
+        $validDate = $dateChecker->checkDate($testEvent->getDate());
+        $this->assertFalse($validDate);
+    }
+
+    public function testDateCheckerWithNotValidQualifyingDate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $dateChecker = $container->get(DateChecker::class);
+        $eventRepository = $container->get(EventRepository::class);
+        $testEvent = $eventRepository->findOneBy(['round' => 0, 'season' => '2022']);
+        $validDate = $dateChecker->checkDate($testEvent->getQualifyingDate());
+        $this->assertFalse($validDate);
+    }
 }
