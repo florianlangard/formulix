73a2452a53ba6fa9496d2a4f1d0c564d9d3e3c11
florianlangard <florian.langard@gmail.com>
2022-06-24 16:32:31 +0200

    added tests for backoffice DriverController

--- tests/Back/DriverControllerTest.php
+++ tests/Back/DriverControllerTest.php
@@ -40,4 +40,69 @@ class DriverControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Driver');
     }
+
+    public function testNewDriver(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('admin@test.com');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/back/driver/new');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Create new Driver');
+
+        $crawler = $client->submitForm('Save', [
+            'driver[fullname]' => 'Didier Test',
+            'driver[number]' => 21,
+            'driver[driver_id]' => 'test',
+            'driver[code]' => 'TST',
+            'driver[isActive]' => true,
+        ]);
+        $this->assertResponseRedirects('/back/driver/');
+    }
+
+    public function testEditDriver(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $driverRepository = static::getContainer()->get(DriverRepository::class);
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('admin@test.com');
+        $testDriver = $driverRepository->findOneBy(['driver_id' => 'test']);
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/back/driver/'.$testDriver->getId().'/edit');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Edit Driver');
+
+        $crawler = $client->submitForm('Update', [
+            'driver[fullname]' => 'modified Test',
+            'driver[number]' => 21,
+            'driver[driver_id]' => 'test',
+            'driver[code]' => 'TST',
+            'driver[isActive]' => true,
+        ]);
+        $this->assertResponseRedirects('/back/driver/');
+    }
+
+    // public function testDeleteDriver(): void
+    // {
+    //     $client = static::createClient();
+    //     $userRepository = static::getContainer()->get(UserRepository::class);
+    //     $driverRepository = static::getContainer()->get(DriverRepository::class);
+        
+    //     // retrieve the test user
+    //     $testUser = $userRepository->findOneByEmail('admin@test.com');
+    //     $testDriver = $driverRepository->findOneBy(['driver_id' => 'test']);
+    //     // simulate $testUser being logged in
+    //     $client->loginUser($testUser);
+    //     $csrfToken = $client->getContainer()->get('security.csrf.token_manager')->getToken('delete')->getValue();
+    //     $client->request('POST', '/back/driver/'.$testDriver->getId(), ['driver' => ['_token' => $csrfToken]]);
+    //     $this->assertResponseRedirects('/back/driver/');
+    // }
 }
